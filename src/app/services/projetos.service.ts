4af65168d47b0f6927e00d392f7428b717ca0a03
joao <joao@platon.com.br>
2023-08-18 17:01:59 -0300

    Adicionado catch no service

--- src/app/services/projetos.service.ts
+++ src/app/services/projetos.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Produto, Projeto, Projetos } from '../Interfaces/IProjetos';
-import { Observable, Subject } from 'rxjs';
+import { Observable, Subject, catchError, throwError } from 'rxjs';
 
 
 @Injectable({
@@ -12,29 +12,59 @@ export class ProjetosService {
   private urlProjetos = "http://localhost:5086/api";
   constructor(private http: HttpClient) { }
 
+  // Buscar todos os projetos paginados da api.
   buscarProjetos(pagina: number, resultado?: number): Observable<Projetos> {
     return this.http.get<Projetos>(`${this.urlProjetos}/projetos/${pagina}`)
   }
- 
+  // Metodo que estará realizando chamada do projeto pelo id
   buscarProjeto(id: number): Observable<Projeto> {
     return this.http.get<Projeto>(`${this.urlProjetos}/projeto/${id}`)
+    .pipe(
+      catchError(
+        (err) => {
+          console.log("Um erro ocorreu ao tentar requisitar o backend");
+          console.error(err);
+          return throwError(err);
+        }
+      )
+    )
   }
- 
+  // Metodo que relaiza chamada a api para relaizar operação de delete.
   remover(id: string): Observable<number> {
     let httpHeader = new HttpHeaders({
       'Content-type': 'application/Json',
       'Access-Control-Allow-Origin' : '*'
     })
     return this.http.delete<number>(`${this.urlProjetos}/delete/${id}`, { headers: httpHeader})
+    .pipe(
+      catchError(
+        (err) => {
+          console.log("Um erro ocorreu ao tentar requisitar o backend");
+          console.error(err);
+          return throwError(err);
+        }
+      )
+    )
   }
- 
+  //Metodo de busca para produtos que irão ser disponibilizados na pagina de cirar produtos.  
   buscarTodosProdutos(): Observable<Produto[]>{
     let httpHeader = new HttpHeaders({
       'Content-type': 'application/Json',
       'Access-Control-Allow-Origin' : '*'
     })
     return this.http.get<Produto[]>(`${this.urlProjetos}/produtosEmEstoque`, {headers: httpHeader})
+    .pipe(
+      catchError(
+        (err) => {
+          console.log("Um erro ocorreu ao tentar requisitar o backend");
+          console.error(err);
+          return throwError(err);
+        }
+      )
+    )
   }
+
+  // Realizado criação de listener e filter para futuramente observar
   private _listerner = new Subject<any>();
   listen() : Observable<any> {
     return this._listerner.asObservable();
