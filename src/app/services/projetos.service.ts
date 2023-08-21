7a397de325a3ab48167485048ef03b8949ecfd30
joao <joao@platon.com.br>
2023-08-21 17:40:48 -0300

    Ajustado metodo de put

--- src/app/services/projetos.service.ts
+++ src/app/services/projetos.service.ts
@@ -8,28 +8,30 @@ import { Observable, Subject, catchError, throwError } from 'rxjs';
   providedIn: 'root'
 })
 export class ProjetosService {
-
+  
   private urlProjetos = "http://localhost:5086/api";
+
+  /** Definiando header padrão para requisições */
+  httpHeader = new HttpHeaders({
+    'Content-type': 'application/Json',
+    'Access-Control-Allow-Origin': '*'
+  })
+  /** Final header options */
+
+  
   constructor(private http: HttpClient) { }
 
-  // Buscar todos os projetos paginados da api.
   buscarProjetos(pagina: number, resultado?: number): Observable<Projetos> {
     return this.http.get<Projetos>(`${this.urlProjetos}/projetos/${pagina}`)
   }
-  // Metodo que estará realizando chamada do projeto pelo id
+
   buscarProjeto(id: string): Observable<Projeto> {
     return this.http.get<Projeto>(`${this.urlProjetos}/projeto/${id}`)
   }
-  // Metodo que relaiza chamada a api para relaizar operação de delete.
   remover(id: string): Observable<number> {
-    let httpHeader = new HttpHeaders({
-      'Content-type': 'application/Json',
-      'Access-Control-Allow-Origin': '*'
-    })
-    return this.http.delete<number>(`${this.urlProjetos}/delete/${id}`, { headers: httpHeader })
+    return this.http.delete<number>(`${this.urlProjetos}/delete/${id}`, { headers: this.httpHeader })
   }
 
-  //Metodo de busca para produtos que irão ser disponibilizados na pagina de cirar produtos.  
   buscarTodosProdutos(): Observable<Produto[]> {
     let httpHeader = new HttpHeaders({
       'Content-type': 'application/Json',
@@ -38,23 +40,22 @@ export class ProjetosService {
     return this.http.get<Produto[]>(`${this.urlProjetos}/produtosEmEstoque`, { headers: httpHeader })
   }
 
-  // Metodo para realizar a criação de um novo projeto.
   novoProjeto(projeto: CriarProjetoDTO): Observable<CriarProjetoDTO> {
     return this.http.post<CriarProjetoDTO>(`${this.urlProjetos}/Create`, projeto);
   }
 
-  //Metodo para update de status
   novoStatus(status: string, id: number){
     let parames = new HttpParams().append("model", status);
-    return this.http.put(`${this.urlProjetos}/update/${id}`, {params: parames});
+    return this.http.put(`${this.urlProjetos}/update/${id}`, status, {params: parames});
   }
 
-  // Realizado a criação de um observable.
+  /** Criação de observable para disparo de eventos delete | error  */
   private _listerner = new Subject<any>();
   listen(): Observable<any> {
     return this._listerner.asObservable();
   }
   filterSub(filter: string) {
     this._listerner.next(filter);
-  }
+ 
+ /** Final Criação de observable */ }
 }
