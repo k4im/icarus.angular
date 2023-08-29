573659b5be05b2a3bfe78f885d0b0ff6e6f79e9e
joao <joao@platon.com.br>
2023-08-29 12:42:25 -0300

    Removido campo de usuario

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrlRoot: "http://localhost:5086/api"
-
+    apiUrlRoot: "http://localhost:5086/api",
+    apiUrlAuth: "http://localhost:5112/api/v0.1/Usuario"
 };
